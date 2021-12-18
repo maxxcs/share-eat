4b36f9eff6dd71f14712d6f4bd25379a64581ef5
Max <max.carlesso@hotmail.com>
2021-12-18 14:40:41 -0300

    fix(pages): import NewDishButton properly

--- src/pages/restaurant/[id].tsx
+++ src/pages/restaurant/[id].tsx
@@ -1,30 +1,42 @@
 import Head from "next/head";
 import { fetchRestaurantById } from "../../utils/fetch";
 import DishesList from "../../components/restaurant/DishesList";
-import ModalHandler from "../../components/modal/ModalHandler";
+import NewDishButton from "../../components/restaurant/NewDishButton";
+import PageNotFound from "../404";
+import { useContext, useEffect } from "react";
+import AppContext from "../../components/Context";
 import type { GetStaticPaths, GetStaticProps } from "next";
 import type { ParsedUrlQuery } from "querystring";
 import type { IRestaurant } from "../../utils/types";
-import PageNotFound from "../404";
 
 interface IRestaurantProps {
+  id: string;
   restaurant: IRestaurant;
 }
 
-function Restaurant({ restaurant }: IRestaurantProps) {
-  if (!restaurant) return PageNotFound;
+function Restaurant({ id, restaurant }: IRestaurantProps) {
+  const { state, setState } = useContext(AppContext);
 
-  return (
-    <>
-      <Head>
-        <title>{restaurant?.name} | Share Eat</title>
-      </Head>
-      <main className="f-col ctr">
-        <DishesList name={restaurant?.name} dishes={restaurant?.menuItems} />
-      </main>
-      <ModalHandler />
-    </>
-  );
+  useEffect(() => {
+    setState({
+      ...state,
+      actualPlace: { id: parseInt(id), name: restaurant.name },
+    });
+  }, []);
+
+  if (!restaurant) return PageNotFound;
+  else
+    return (
+      <>
+        <Head>
+          <title>{restaurant?.name} | Share Eat</title>
+        </Head>
+        <main className="f-col ctr">
+          <DishesList name={restaurant?.name} dishes={restaurant?.menuItems} />
+        </main>
+        <NewDishButton />
+      </>
+    );
 }
 
 interface IParams extends ParsedUrlQuery {
@@ -44,7 +56,7 @@ export const getStaticProps: GetStaticProps = async (context) => {
   const restaurant = await fetchRestaurantById(id);
 
   return {
-    props: { restaurant },
+    props: { id, restaurant },
   };
 };
 
