8e0ebd6edc93f86e42225608ce1ab23ba2f86eaa
Max <max.carlesso@hotmail.com>
2021-12-18 12:12:35 -0300

    feat(components): add footer component

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.css";
+
+function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <strong>Made with ♥ by Max</strong>
+    </footer>
+  );
+}
+
+export default Footer;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import "../styles/globals.css";
 import Header from "../components/layout/Header";
+import Footer from "../components/layout/Footer";
 import type { AppProps } from "next/app";
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <div className="f-col ctr">
       <Header displayBg={true} />
+      <Footer />
     </div>
   );
 }
