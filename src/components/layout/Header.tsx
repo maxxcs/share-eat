17254065b7543c8bc182a7eff61aa42a84a936bd
Max <max.carlesso@hotmail.com>
2021-12-18 12:11:39 -0300

    feat(components): add header component

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,45 @@
+import Image from "next/image";
+import Link from "next/link";
+import styles from "./Header.module.css";
+
+interface IHeaderProps {
+  displayBg: boolean;
+}
+
+function Header({ displayBg }: IHeaderProps) {
+  const bgComponent = displayBg ? (
+    <div className={`f-row ctr ${styles.background}`}>
+      <Image
+        src="/background-effect.svg"
+        alt="Share Eat"
+        width={455}
+        height={227}
+      />
+    </div>
+  ) : null;
+
+  return (
+    <header className={`f-col ctr ${styles.container}`}>
+      <div className="f-row">
+        <button className={`f-row ctr ${styles.back}`} type="button">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="18"
+            height="18"
+            viewBox="0 0 24 24"
+          >
+            <path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z" />
+          </svg>
+        </button>
+        <Link href="/">
+          <a>
+            <Image src="/logo.svg" alt="Share Eat" width={96} height={18} />
+          </a>
+        </Link>
+      </div>
+      {bgComponent}
+    </header>
+  );
+}
+
+export default Header;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,11 @@
 import "../styles/globals.css";
+import Header from "../components/layout/Header";
 import type { AppProps } from "next/app";
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <div className="f-col ctr">
+      <Header displayBg={true} />
     </div>
   );
 }
