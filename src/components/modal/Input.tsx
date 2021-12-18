fe262e69dbec1770ba0673a329bfecd3d4f19237
Max <max.carlesso@hotmail.com>
2021-12-18 19:00:14 -0300

    feat(Input): make the component more generic

--- src/components/modal/Input.tsx
+++ src/components/modal/Input.tsx
@@ -1,16 +1,23 @@
+import { CSSProperties, HTMLProps } from "react";
 import styles from "./Input.module.css";
 
 interface IInputProps {
+  type: string;
   name: string;
-  placeholder?: string;
   value?: string;
+  wrapper?: { style: CSSProperties; text: string };
+  props?: HTMLProps<HTMLInputElement>;
+  [key: string]: any;
 }
 
-function Input({ name, placeholder, value }: IInputProps) {
+function Input({ type, name, value, wrapper, ...props }: IInputProps) {
   return (
     <label className={`f-col ${styles.container}`}>
       <span>{name}</span>
-      <input type="text" value={value} placeholder={placeholder} />
+      <div className="f-row ctr" style={wrapper?.style}>
+        <span>{wrapper?.text}</span>
+        <input type={type} value={value} {...props} />
+      </div>
     </label>
   );
 }
